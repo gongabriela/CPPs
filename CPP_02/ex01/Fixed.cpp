e98243d4af33f6908bf9d4e54368acf18f548df9
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-01-23 20:19:12 +0000

    adding cpp02/ex00 and cpp02/ex01

---
+++ CPP_02/ex01/Fixed.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggoncalv <ggoncalv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 21:11:30 by ggoncalv          #+#    #+#             */
+/*   Updated: 2026/01/15 21:11:30 by ggoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+Fixed::Fixed() : _fixedPointValue(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int n) {
+    std::cout << "Int constructor called" << std::endl;
+    _fixedPointValue = n << _fractionalBits;
+}
+
+Fixed::Fixed(const float f) {
+    std::cout << "Float constructor called" << std::endl;
+    _fixedPointValue = roundf(f * (1 << _fractionalBits));
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& src) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &rhs)
+        _fixedPointValue = rhs.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return _fixedPointValue;
+}
+
+void    Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    _fixedPointValue = raw;
+}
+
+int Fixed::toInt(void) const {
+    return _fixedPointValue >> _fractionalBits;
+}
+
+float Fixed::toFloat(void) const {
+    return (float)_fixedPointValue / (1 << _fractionalBits);
+}
+
+std::ostream& operator<<(std::ostream &out, const Fixed &fixed) {
+    out << fixed.toFloat();
+    return out;
+}
---
+++ CPP_02/ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggoncalv <ggoncalv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 21:11:57 by ggoncalv          #+#    #+#             */
+/*   Updated: 2026/01/15 21:11:57 by ggoncalv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main (void) {
+
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+
+    a = Fixed(1223.4321f);
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return 0;
+}
